56cfa031e24e83552bd679c5a3c7cfb475e6af23
Shailesh Chandrakant Patil <scpatil2004@gmail.com>
2026-07-25 13:49:52 +0530

    Time: 3 ms (66.21%), Space: 47.5 MB (65.39%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,22 +1,24 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] ans = new int[2];
-        int i = 0 ;
-        while(i < nums.length){
-            int correct = nums[i]-1;
-            if(nums[i]!=nums[correct]){
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+
+            if (nums[i] != nums[correct]) {
                 int temp = nums[i];
                 nums[i] = nums[correct];
-                nums[correct]= temp;
-            }
-            else{
-                if(i!=correct){
-                     ans[0]=nums[i];
-                     ans[1]= i+1;
-                }
+                nums[correct] = temp;
+            } else {
                 i++;
             }
         }
-        return ans;
+
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return new int[] { nums[i], i + 1 };
+            }
+        }
+
+        return new int[] { -1, -1 };
     }
 }
