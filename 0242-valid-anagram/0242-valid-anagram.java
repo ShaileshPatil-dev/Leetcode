afe71f71c02d1a55b543b7c02325ad0180144374
Shailesh Chandrakant Patil <scpatil2004@gmail.com>
2026-08-16 15:28:07 +0530

    Time: 18 ms (13.37%), Space: 46.7 MB (16.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,19 +3,34 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
+        HashMap<Character , Integer> mapS = new HashMap<>();
+        HashMap<Character , Integer> mapT = new HashMap<>();
 
-        int[] freq = new int[26];
-        for(int i = 0 ; i < s.length() ;i++){
-            freq[s.charAt(i)-'a']++;
-            freq[t.charAt(i)-'a']--;
-        }
-
-        for(int j = 0 ; j < freq.length ; j++){
-            if(freq[j]!=0){
-                return false;
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            if(mapS.containsKey(ch)){
+                mapS.put(ch,mapS.get(ch)+1);
             }
+            else{
+                mapS.put(ch,1);
+            }
+
         }
-        return true;
+
+        for(int i = 0 ; i < t.length() ; i++){
+            char ch = t.charAt(i);
+            if(mapT.containsKey(ch)){
+                mapT.put(ch,mapT.get(ch)+1);
+            }
+            else{
+                mapT.put(ch,1);
+            }
+
+        }
+
+        if(mapS.equals(mapT)){
+            return true;
+        }
+        return false;
     }
 }
-
