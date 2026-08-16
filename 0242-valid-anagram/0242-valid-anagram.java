d9fda942dc1d35db6b5415afc865c134cd285e7e
Shailesh Chandrakant Patil <scpatil2004@gmail.com>
2026-08-16 18:08:26 +0530

    Time: 23 ms (5.79%), Space: 44.5 MB (60.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,36 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
+        if(s.length()!=t.length()){
             return false;
         }
-        HashMap<Character , Integer> mapS = new HashMap<>();
-        HashMap<Character , Integer> mapT = new HashMap<>();
-
-        for(int i = 0 ; i < s.length() ; i++){
-            char ch = s.charAt(i);
-            if(mapS.containsKey(ch)){
-                mapS.put(ch,mapS.get(ch)+1);
-            }
-            else{
-                mapS.put(ch,1);
-            }
-
+        HashMap<Character , Integer> map = new HashMap<>();
+        for(int i = 0 ; i < t.length();i++){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            map.put(ch1,map.getOrDefault(ch1,0)+1);
+            map.put(ch2,map.getOrDefault(ch2,0)-1);
         }
-
-        for(int i = 0 ; i < t.length() ; i++){
-            char ch = t.charAt(i);
-            if(mapT.containsKey(ch)){
-                mapT.put(ch,mapT.get(ch)+1);
-            }
-            else{
-                mapT.put(ch,1);
-            }
-
-        }
-
-        if(mapS.equals(mapT)){
-            return true;
-        }
+        for (int count : map.values()) {
+        if (count != 0) {
         return false;
+    }
+}
+return true;
     }
 }
