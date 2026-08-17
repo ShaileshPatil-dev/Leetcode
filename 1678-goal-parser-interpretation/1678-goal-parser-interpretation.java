5e3ed290852325910339cd0a16a89dfe0432f5d2
Shailesh Chandrakant Patil <scpatil2004@gmail.com>
2026-08-18 00:31:17 +0530

    Time: 1 ms (75.26%), Space: 43.1 MB (32.73%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -11,8 +11,7 @@ class Solution {
                 ans.append('o');
                 }
                 else{
-                    ans.append('a');
-                ans.append('l');
+                    ans.append("al");
                 }
             }
         }
