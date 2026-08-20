6f5c18a3c70ac14baa8be7c39158f5a9896eff8b
Shailesh Chandrakant Patil <scpatil2004@gmail.com>
2026-08-20 18:15:09 +0530

    Time: 0 ms (100%), Space: 42.7 MB (84.32%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,17 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
-        String ans = new String();
-        // String ans = s.toLowerCase();
-        for(int i = 0 ; i< s.length() ;i++){
-            int ch =(int) s.charAt(i);
-            if(ch>=65 && ch <=90){
-                ch += 32;
-                ans += (char)ch;
-            }
-            else{
-                ans += (char)ch;
-            }
-        }
+        String ans = s.toLowerCase();
         return ans;
     }
 }
