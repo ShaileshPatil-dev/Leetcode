ea4bf70fe1d76e112a7da43fc81f43237ad5dba0
Shailesh Chandrakant Patil <scpatil2004@gmail.com>
2026-08-20 21:45:30 +0530

    Time: 6 ms (51.84%), Space: 45.5 MB (68.58%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-                if(moves.length()%2!=0) {return false;}
+     if(moves.length()%2!=0) {return false;}
         int X = 0 ;
         int Y = 0;
         for(int i = 0 ;i<moves.length();i++){
             char ch = moves.charAt(i);
-            if(ch =='U'){
-                Y++;
-            }
-            if(ch=='D'){
-                Y--;
-            }
-            if(ch=='R'){
-                X++;
-            }
-            if(ch=='L'){
-                X--;
-            }
+            if (ch == 'U') {
+    Y++;
+} else if (ch == 'D') {
+    Y--;
+} else if (ch == 'R') {
+    X++;
+} else {
+    X--;
+}
         }
         return X == 0 && Y == 0;
     }
