e6992fe8beef0eca3424bcb819751604d63014eb
Shailesh Chandrakant Patil <scpatil2004@gmail.com>
2026-08-30 21:45:46 +0530

    Time: 0 ms (100%), Space: 47.6 MB (20.18%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,23 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int i = 0 ;
-        while(i < nums.length)
-        {
-            int correctposition = nums[i];
-            if(nums[i]<nums.length && nums[i]!= nums[correctposition]){
-                int temp = nums[correctposition];
-                nums[correctposition]= nums[i];
-                nums[i]= temp;
-            }
-            else{
-                i++;
-            }
+        int ans = nums.length;
+    for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ i ^ nums[i];
         }
-        for(i=0;i<nums.length ; i++){
-            if(nums[i]!= i){
-                return i;
-            }
-        }
-        return nums.length;
+        return ans;
     }
 }
