2d3034185d0abc09195056349abfbb7d4f823791
Shailesh Chandrakant Patil <scpatil2004@gmail.com>
2026-08-30 21:34:38 +0530

    Time: 1 ms (60.92%), Space: 42.3 MB (84.61%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+       for(int i = 0 ; i< 32 ; i++) {
+        ans = ans << 1;
+        int rightmost = n & 1 ;
+        ans = ans | rightmost ;
+        n = n >> 1;
+       }
+       return ans;
+    }
+}
